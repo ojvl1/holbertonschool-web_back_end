05b2b78edaaaabf2b685b1e0996ae164fe6680df
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-24 11:47:22 -0400

    change comment

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" Basics annotations"""
+""" Basics annotations - add """
 def add(a: float, b:float):
-    """Function that return the sum of two arguments"""
+    """ Function that return the sum of two arguments """
     return a + b
