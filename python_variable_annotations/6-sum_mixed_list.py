f7a39585b601241bb543578149428fca4bc04e2e
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-25 11:35:37 -0400

    importing Union and fix code

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Complex types - mixed list """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Function that return their sum as a float """
     return sum(mxd_lst)
