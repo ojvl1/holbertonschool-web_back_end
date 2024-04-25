4738e11c57e450a8ffa709134cd8144d5e4dd4fd
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-25 13:57:23 -0400

    Complex types - functions

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Complex types - functions """
+from collections.abc import Callable
+
+
+def make_multiplier(multiplier: float) -> float:
+    """ Function that return a function that multiplies a float"""
+    return callable(make_multiplier)
