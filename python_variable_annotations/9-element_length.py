5a72590bb3ddf58a09f627b09a63b653a0b7848c
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-25 15:01:15 -0400

    Return a list of tuples

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pyhton3
+""" Function element_length that takes a list
+    lst of strings as argument and returns a list
+    of integers representing the lengths of the
+    corresponding strings
+"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Return a list of tuples """
+    return [(i, len(i)) for i in lst]
