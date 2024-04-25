b426a0b985f1673508bcc2e7510d2f183b096792
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-25 11:03:13 -0400

    import List form typing and fix the code

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Complex types - list of floats """
+from typing import List
 
 
-def sum_list(input__list: list[float]) -> float:
+def sum_list(input__list: List[float]) -> float:
     """ Function that take a list of floats as argument and returns their sum as a float """
     return sum(input__list)
