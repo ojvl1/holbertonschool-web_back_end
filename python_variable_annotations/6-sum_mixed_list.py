10d4d77a89a7160d165740de5f9791972455069c
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-25 11:29:33 -0400

    importn List and fix the code

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Complex types - mixed list """
+from typing import List
 
 
-def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
     """ Function that return their sum as a float """
     return sum(mxd_lst)
