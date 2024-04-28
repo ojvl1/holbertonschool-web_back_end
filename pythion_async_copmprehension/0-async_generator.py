c97c52597bb93d430a3b3484d38a4e8f460d1728
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-27 22:29:55 -0400

    change module

--- pythion_async_copmprehension/0-async_generator.py
+++ pythion_async_copmprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 '''
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     '''
         Coroutine async_generator that takes no arguments.
         The coroutine will loop 10 times, each time asynchronously wait 1
