aa80adb827cb898e464d49fefe4b4d8ace153f94
ojvl1 <antompietriosvaldo@gmail.com>
2024-05-09 13:51:56 -0400

    change code

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,10 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const idx of array) {
-    let value = array[idx];
-    array[idx] = appendString + value;
+    let value = idx;
+    value = appendString + value;
     newArray.push(value);
   }
 
-  return array;
+  return newArray;
 }
