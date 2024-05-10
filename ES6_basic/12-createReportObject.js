e93f40aa8b1ebb2ad7c14f7767e26c31f8513c68
ojvl1 <antompietriosvaldo@gmail.com>
2024-05-09 20:59:54 -0400

    fix code

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,10 @@
 export default function createReportObject(employeesList) {
-    const myObject = {
-      allEmployees: employeesList,
-      getNumberOfDepartments(allEmployees) {
-        const totalDepartments = Object.keys(allEmployees);
-        return totalDepartments.length;
-      },
-    };
-    return myObject;
-  }
+  const myObject = {
+    allEmployees: employeesList,
+    getNumberOfDepartments(allEmployees) {
+      const totalDepartments = Object.keys(allEmployees);
+      return totalDepartments.length;
+    },
+  };
+  return myObject;
+}
