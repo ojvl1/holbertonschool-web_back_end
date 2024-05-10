8ee1c00e4996a814ca9173e337e00664512a1ab1
ojvl1 <antompietriosvaldo@gmail.com>
2024-05-10 15:10:51 -0400

    using Symbol

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('Name must be a string');
+    }
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('Code must be a string');
+    }
+  }
+
+  // Getter to overwrite .toString() all objects have this
+  // Is like the __str__ representation from python
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
