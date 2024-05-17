eac551b223d100ccb879e03a33f9069f7de353be
ojvl1 <carlosantompietri175@gmail.com>
2024-05-17 15:46:22 -0400

    fix

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
-export default function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI (success) {
   const myPromise = new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       const error = new Error('The fake API is not working currently');
-      reject (error);
+      reject(error);
     }
   });
   return myPromise;
