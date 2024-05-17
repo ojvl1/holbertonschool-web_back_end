291a4d6dd7803224a4b4338d047d008468d806ab
ojvl1 <carlosantompietri175@gmail.com>
2024-05-17 18:05:58 -0400

    change code

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-    return promise
+  return promise
     .then(() => {
-    console.log('Got a response from the API');
-    return {
+      console.log('Got a response from the API');
+      return {
         status: 200,
         body: 'success',
-    };
+      };
     })
     .catch(() => {
-    console.log('Got a response from the API');
-    return new Error();
+      console.log('Got a response from the API');
+      return new Error();
     });
 }
