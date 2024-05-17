25f4eb9112fe23a2563a46c97a65b474e7b6c64f
ojvl1 <carlosantompietri175@gmail.com>
2024-05-17 15:28:03 -0400

    lint

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
     if (success) {
-      resolve({status: 200, body: 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else {
       const error = new Error('The fake API is not working currently');
       reject (error);
