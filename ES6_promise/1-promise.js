66d3121b252de3b2052a6f735529ada4c9bde43c
ojvl1 <carlosantompietri175@gmail.com>
2024-05-17 15:24:09 -0400

    return a promise with a parameter

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success) {
+  const myPromise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({status: 200, body: 'Success'});
+    } else {
+      const error = new Error('The fake API is not working currently');
+      reject (error);
+    }
+  });
+  return myPromise;
+}
