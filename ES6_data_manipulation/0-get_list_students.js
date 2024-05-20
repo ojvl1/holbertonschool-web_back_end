2f572be256ad8aa9d3b718c11f86a3bbed4acdda
ojvl1 <antompietriosvaldo@gmail.com>
2024-05-20 14:27:26 -0400

    fix code

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -3,6 +3,6 @@ export default function getListStudents() {
     {id: 1, firstName: 'Guillaume', location: 'San Fracisco'},
     {id: 2, firstName: 'James', location: 'Columbia'},
     {id: 5, firstName: 'Serena', location: 'San Francisco'}
-  ]
+  ];
   return myArray;
 }
