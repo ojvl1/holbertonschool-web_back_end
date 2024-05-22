f632dff3f6efe2940e18f3d1fa725b8753ebe2dd
ojvl1 <antompietriosvaldo@gmail.com>
2024-05-21 23:21:12 -0400

    change

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
-export default function cleanSet(set, startString) {
+function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
@@ -10,3 +10,4 @@ export default function cleanSet(set, startString) {
   }
   return result.join('-');
 }
+export default cleanSet;
