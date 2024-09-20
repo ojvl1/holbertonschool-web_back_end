cafdce0b7d686c85e161c312487947ccddd08d12
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-19 21:30:50 -0400

    Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from base_caching import BaseCaching
+
+
+"""Basic dictionary"""
+
+
+class BasicCache(BaseCaching):
+    """Defining the functions
+        - put function: assign the dictionary self.cache_data
+        the item value for the key 'key'
+        - get function: return the value in self.cache_data
+        liinked to key
+    """
+    def put(self, key, item):
+        self.cache_data[key] = item
+        if key == None or item == None:
+            return
+
+    def get(self, key):
+        return self.cache_data.get(key)
+        if key == None or key not in self.cache_data:
+            return None
+
+if __name__ == "__main__":
+    BasicCache()
