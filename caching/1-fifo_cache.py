92cfa6d511bae23bd6db1add0178db3b8330c998
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-20 15:01:06 -0400

    FIFO caching

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -5,8 +5,10 @@ from base_caching import BaseCaching
 """FIFO caching"""
 
 
-class FIFICache(BaseCaching):
-    """Defining functions"""
+class FIFOCache(BaseCaching):
+    """Defining functions
+        - put function: implementing FIFO algorithm
+    """
 
     def __init__(self):
         super().__init__()
@@ -14,4 +16,16 @@ class FIFICache(BaseCaching):
     def put(self, key, item):
         if key is None or item is None:
             return
+
         self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_item = next(iter(self.cache_data))
+            print(f"Discard: {first_item}")
+            self.cache_data.pop(first_item)
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data.get(key)
