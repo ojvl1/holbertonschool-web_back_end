5bd9c31fcd0f8245397581a4dab2bb8da095bd5f
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-20 15:11:41 -0400

    make some chages

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from base_caching import BaseCaching
-
-
 """Basic dictionary"""
+
+
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -20,6 +20,7 @@ class BasicCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
+        """return value of the key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
