7753407de6340cb43651639fcfd3258afe9dee56
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-19 22:23:26 -0400

    change code

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -15,11 +15,11 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
-        self.cache_data[key] = item
         if key is None or item is None:
             return
+        self.cache_data[key] = item
 
     def get(self, key):
-        return self.cache_data.get(key)
         if key is None or key not in self.cache_data:
             return None
+        return self.cache_data.get(key)
