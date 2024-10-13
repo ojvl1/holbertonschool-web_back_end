3841d6278dcb61326c1237057468e1c73bc8d525
ojvl1 <antompietriosvaldo@gmail.com>
2024-10-13 12:15:30 -0400

    .

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 """Function that returns the log message obfuscated"""
-
+"""
+    fields: a list of strings representing all fields to obfuscate
+    ---------------------------------------------------------------------
+    redaction: a string representing by what the field will be obfuscated
+    ---------------------------------------------------------------------
+    message: a string representing the log line
+    ---------------------------------------------------------------------
+    separator: a string representing by which character is separating all
+    fields in the log line
+    """
 
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """
-        fields: a list of strings representing all fields to obfuscate
-        ---------------------------------------------------------------------
-        redaction: a string representing by what the field will be obfuscated
-        ---------------------------------------------------------------------
-        message: a string representing the log line
-        ---------------------------------------------------------------------
-        separator: a string representing by which character is separating all
-        fields in the log line
-    """
-
     pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
     return re.sub(
         pattern, lambda m: m.group(0).split('=')[0] + f'={redaction}', message)
