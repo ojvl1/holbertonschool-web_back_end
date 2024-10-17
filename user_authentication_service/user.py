40d483c98691558277fe439d7035d86d3a47413f
ojvl1 <antompietriosvaldo@gmail.com>
2024-10-17 14:07:38 -0400

    pycodestyle

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-
+"""
+  create a SQLAlchemy model named User for a database table named users
+  using the mapping declaration of SQLAlchemy
+"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-
 Base = declarative_base()
 
-class User(Base):
-  __tablename__ = 'users'
 
-  id = Column(Integer, primary_key=True)
-  email = Column(String(250), nullable=False)
-  hashed_password = Column(String(250), nullable=False)
-  session_id = Column(String(250), nullable=False)
-  reset_token = Column(String(250), nullable=False)
+class User(Base):
+    """
+    2The model will have the following attributes:
+    id, the integer primary key
+    email, a non-nullable string
+    hashed_password, a non-nullable string
+    session_id, a nullable string
+    reset_token, a nullable string
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
