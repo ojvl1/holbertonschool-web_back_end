ceeedc12edce4d329864643b7a8604bd5efc53a1
ojvl1 <antompietriosvaldo@gmail.com>
2024-10-22 22:21:16 -0400

    fix name method

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -26,7 +26,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_exception(self, nested_map, path):
+    def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
