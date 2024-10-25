7bbbc8e6402932fc957642b7b1f8efa60c8d2613
ojvl1 <antompietriosvaldo@gmail.com>
2024-10-25 13:33:50 -0400

    add comment  and fix pycodestyle

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
+"""
+Create a basic Flask template
+"""
 
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
+    """
+    Rendering template
+    """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
