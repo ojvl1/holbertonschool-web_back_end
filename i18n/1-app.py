eb47c341a0ac3763f4963e468f1691a95f00c08d
ojvl1 <carlosantompietri175@gmail.com>
2024-10-27 17:35:32 -0400

    fix

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -22,12 +22,6 @@ class Config():
 app.config.from_object(Config)
 
 
-@babel.localeselector
-def get_locale():
-    ''' return best languages '''
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 @app.route('/')
 def home():
     """
