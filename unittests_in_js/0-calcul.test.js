a050cca7a006e9bd21edcad390d72369909e72aa
ojvl1 <antompietriosvaldo@gmail.com>
2024-11-12 22:13:49 -0400

    Take out describe method

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,20 +1,18 @@
 const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
-describe("calculateNumber", () => {
-  it("...", () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
+it("...", () => {
+  assert.strictEqual(calculateNumber(1, 3), 4);
+});
 
-  it("...", () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
+it("...", () => {
+  assert.strictEqual(calculateNumber(1, 3.7), 5);
+});
 
-  it("...", () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
+it("...", () => {
+  assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+});
 
-  it("...", () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+it("...", () => {
+  assert.strictEqual(calculateNumber(1.5, 3.7), 6);
 });
