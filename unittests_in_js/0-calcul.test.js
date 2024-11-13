8c7d1f04e4559b5d674184dd6cda977f0814dee4
ojvl1 <antompietriosvaldo@gmail.com>
2024-11-12 21:10:57 -0400

    change the message output of test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,19 +2,19 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", () => {
-  it("should round both numbers down and return the sum", () => {
+  it("...", () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it("should round both numbers up and return the sum", () => {
+  it("...", () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it("should round one number down and one number up, and return the sum", () => {
+  it("...", () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it("should work when one number is exactly .5", () => {
+  it("...", () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
