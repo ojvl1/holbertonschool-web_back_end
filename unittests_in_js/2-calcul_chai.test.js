2c54bef63bbbebb608c2c57635b164b8af9fefcc
ojvl1 <antompietriosvaldo@gmail.com>
2024-11-13 20:30:26 -0400

    fix code: 'NUM' -> 'SUM'

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -3,13 +3,13 @@ const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber", () => {
   it("...", () => {
-    expect(calculateNumber("NUM", 1.4, 4.5)).to.equal(6);
+    expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
   });
   it("...", () => {
     expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
   });
   it("...", () => {
-    expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+    expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.be.closeTo(0.2, 0.001);
   });
   it("...", () => {
     expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
