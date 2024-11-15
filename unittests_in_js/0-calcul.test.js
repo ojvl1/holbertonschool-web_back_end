ec118afd8a7b22c5a825cbcee8ceb8204bc4b080
ojvl1 <antompietriosvaldo@gmail.com>
2024-11-15 15:54:43 -0400

    Delete the other cases

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -17,16 +17,4 @@ describe("calculateNumber", () => {
   it("should return 6 when inputs are 1.5 and 3.7", () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-
-  it("should return 0 when inputs are 0 and 0", () => {
-    assert.strictEqual(calculateNumber(0, 0), 0);
-  });
-
-  it("should return -2 when inputs are -1.4 and -0.5", () => {
-    assert.strictEqual(calculateNumber(-1.4, -0.5), -2);
-  });
-
-  it("should return -3 when inputs are -1.6 and -0.5", () => {
-    assert.strictEqual(calculateNumber(-1.6, -0.5), -3);
-  });
 });
