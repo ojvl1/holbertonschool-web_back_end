758be39446ddf5e5a688ce53823f4da39ca6c63e
ojvl1 <antompietriosvaldo@gmail.com>
2024-11-18 15:28:40 -0400

    comments

--- queuing_system_in_js/1-redis_op.js
+++ queuing_system_in_js/1-redis_op.js
@@ -10,10 +10,25 @@ client.on("error", (err) => {
   console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
+/**
+ * Sets a value in Redis for the given key
+ * @param {string} schoolName - The key
+ * @param {string} value - The value to set
+ */
 function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, print);
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error(`Error setting value for ${schoolName}: ${err.message}`);
+    } else {
+      console.log(`Reply: ${reply}`);
+    }
+  });
 }
 
+/**
+ * Retrieves the value for a given key from Redis and logs it
+ * @param {string} schoolName - The key to fetch the value for
+ */
 function displaySchoolValue(schoolName) {
   client.get(schoolName, (err, value) => {
     if (err) {
@@ -24,6 +39,7 @@ function displaySchoolValue(schoolName) {
   });
 }
 
+// Function calls
 displaySchoolValue("Holberton");
 setNewSchool("HolbertonSanFrancisco", "100");
 displaySchoolValue("HolbertonSanFrancisco");
